5a399f99e0499b196ebbbe838a5b5ca49e805fdf
Gessiel Silva <taimoor@naj.ae>
2024-06-18 20:26:25 -0300

    alteração do banco de dados

--- bd.sql
+++ bd.sql
@@ -1,5 +1,14 @@
 CREATE DATABASE dbpoo;
 USE dbpoo;
+
+CREATE TABLE `dbpoo`.`login` (
+  `idlogin` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(150) NOT NULL,
+  `senha` VARCHAR(150) NOT NULL,
+  PRIMARY KEY (`idlogin`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
 CREATE TABLE usuario (
   id_usuario INT NOT NULL AUTO_INCREMENT,
   nome VARCHAR(150) NOT NULL,
@@ -13,13 +22,7 @@ CREATE TABLE usuario (
   FOREIGN KEY (idLogin) REFERENCES login(idLogin)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `dbpoo`.`login` (
-  `idlogin` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(150) NOT NULL,
-  `senha` VARCHAR(150) NOT NULL,
-  PRIMARY KEY (`idlogin`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+
 
 
 /*
