f72b7cc4e3b3f63d3f187d48984f79551ee87cc6
Lucas Cainã <caina.amhp@gmail.com>
2024-06-19 16:51:48 -0300

    [FEAT] deletar e alterar usuario

--- model/DAO/ClassUsuarioDAO.php
+++ model/DAO/ClassUsuarioDAO.php
@@ -24,9 +24,41 @@ class UsuarioDAO extends Connect
         }    
     }
 
+    public function updateUsuario(Usuario $usuario)
+    {
+        $sql = "UPDATE usuario SET nome = ?, cpf = ?, rg = ?, endereco = ?, email = ?, ativo = ?, idLogin = ? WHERE id_usuario = ? ";
+        $statement = $this->connection->prepare($sql);
+        $statementDados = array(
+            $usuario->getNome(),
+            $usuario->getCpf(),
+            $usuario->getRg(),
+            $usuario->getEndereco(),
+            $usuario->getEmail(),
+            $usuario->getAtivo(), 
+            $usuario->getIdUsuario(),
+            $usuario->getIdLogin()
+        );
+        if ($statement->execute($statementDados)) {
+             echo 'Registro atualizado com sucesso';
+             } else {
+                 echo 'Erro ao executar o script no banco';
+                }
+            }
+
+    public function deleteUsuario($id_usuario)
+    {
+        $sql = " UPDATE usuario SET ativo = 0 WHERE $id_usuario ? ";
+        $statement = $this->connection->prepare($sql);
+        if ($statement->execute(array($id_usuario))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public function getUsuarios()
     {
-        $sql = "SELECT * FROM usuario";
+        $sql = "SELECT * FROM usuario where ativo = 1";
         // conectando e preparando o Sql pra rodar no banco
         $statement = $this->connection->prepare($sql);
         // executando no banco
